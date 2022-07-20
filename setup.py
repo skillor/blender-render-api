df2e298589fceecf90b480d79ee6edcf9262ff19
Release Runner <skillor@gmx.de>
2022-07-20 10:36:44 +0000

    Bump release version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(name='Blender Renderer',
       packages=['blender_renderer'],
-      version='1.0',
+      version='1.1',
       description='Render blender scenes',
       author='skillor',
       author_email='skillor@gmx.net',
